e096c9af40c2f34afbc9c2fdeff6ebcd73fb9a20
Cory Fauver <cofauver@gmail.com>
2015-09-20 17:04:55 -0700

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('boilerplateApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
